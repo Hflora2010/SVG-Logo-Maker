ac3d92997a1d5cc1d39aad1e5033d20d74b301fc
Hailey Flora <Haileycflora@gmail.com>
2023-04-07 22:38:07 -0400

    finish shapes.test.js

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,15 +1,25 @@
-const { Triangle, Circle } = require('../lib/Shapes.js')
+const { Triangle, Circle, Polygon, Rectangle } = require('../lib/Shapes.js')
 
-// const shape = new Triangle();
-// shape.setColor("blue");
-// expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
-
-describe('Circle', () => {
-    describe('fillcolor', () => {
-        it('should take in set color and check to see it is correct', () => {
-            const shape = new Circle("fillcolor");
-            shape.display("blue");
-            expect(NEEDAMETHOD.render()).toEqual(`<circle cx="100" cy="100" r="100" fill="blue"/>`);
-        });
+describe("Circle", () => {
+    test("TEST", () => {
+        const expectedSvg = `<circle cx="100" cy="100" r="100" fill="blue"/>`;
+        const circle = new Circle("red", "SVG", "blue");
+        expect(circle.display("blue")).toEqual(expectedSvg);
     });
-});
+  });
+
+  describe("Polygon", () => {
+    test("TEST", () => {
+        const expectedSvg = `<polygon points= "50,0 100,100 0,100" x="14" y="23" fill="blue"/>`;
+        const polygon = new Polygon("red", "SVG", "blue");
+        expect(polygon.display("blue")).toEqual(expectedSvg);
+    });
+  });
+
+  describe("Rectangle", () => {
+    test("TEST", () => {
+        const expectedSvg = `<rect x="10" y="100" width="150" height="150" fill="blue"/>`;
+        const rectangle = new Rectangle("red", "SVG", "blue");
+        expect(rectangle.display("blue")).toEqual(expectedSvg);
+    });
+  });
