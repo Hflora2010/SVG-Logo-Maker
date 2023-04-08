e87787845d3e268c52f5c7774fd3b749ad986951
Hailey Flora <Haileycflora@gmail.com>
2023-04-07 21:07:09 -0400

    add tests

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,3 +1,15 @@
-const shape = new Triangle();
-shape.setColor("blue");
-expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+const { Triangle, Circle } = require('../lib/Shapes.js')
+
+// const shape = new Triangle();
+// shape.setColor("blue");
+// expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+
+describe('Circle', () => {
+    describe('fillcolor', () => {
+        it('should take in set color and check to see it is correct', () => {
+            const shape = new Circle();
+            const circle = shape.fillcolor("blue");
+            expect(shape.render()).toEqual(`<circle cx="100" cy="100" r="100" fill="blue"/>`);
+        });
+    });
+});
